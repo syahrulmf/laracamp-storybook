d2c3001f315b2e268034811264f945c451431a8b
syahrulmf <syahrulmf@gmail.com>
2023-03-06 21:16:05 +0700

    story: atom badge

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,7 +1,23 @@
-type Props = {};
+import { ReactNode } from "react";
+import configs from "./configs";
 
-function Badge({}: Props) {
-  return <div>Badge</div>;
+type Props = {
+  children: ReactNode;
+  state: "success" | "warning" | "danger";
+  className?: string;
+};
+
+function Badge({ children, state, className }: Props) {
+  const combineClassName = [
+    "py-1 px-4 rounded-full inline-flex items-center",
+    configs?.state?.[state] || "",
+    className || "",
+  ];
+  return <span className={combineClassName.join(" ")}>{children}</span>;
 }
+
+// Badge.defaultProps: {
+//   state:
+// }
 
 export default Badge;
---
+++ src/stories/Badge/example/Sample.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Badge from "../../../components/Badge";
+
+type Props = {};
+
+function Sample({}: Props) {
+  return <Badge state="success">Sample</Badge>;
+}
+
+export default Sample;
