4c581d25053070eeb0365519a512d4c4dc13f6ff
syahrulmf <syahrulmf@gmail.com>
2023-03-11 00:03:49 +0700

    component ready

---
+++ dist/components/Button/configs.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const configs = {
+    state: {
+        primary: "border-2 border-transparent bg-purple hover:bg-navy focus:bg-navy active:bg-navy text-white",
+        secondary: "border-2 border-soft-purple2 bg-soft-purple text-purple hover:border-transparent hover:bg-navy focus:bg-navy active:bg-navy hover:text-white",
+        alternative: "",
+        disabled: "border-2 border-transparent bg-soft-grey text-grey",
+        loading: "border-2 border-transparent bg-soft-grey text-grey",
+    },
+};
+exports.default = configs;
